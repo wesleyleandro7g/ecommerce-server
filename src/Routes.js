0c9dc29ba740b19abea732988511f5748a51220e
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2020-05-27 19:16:01 -0300

    Alteracoes nas rotas

--- src/Routes.js
+++ src/Routes.js
@@ -10,18 +10,19 @@ const ProductController = require("./controllers/ProductController");
 //### Rotas para autenticar usuários
 app.post("/authentication", AuthController.authenticate);
 
-//### Rotas para operações com usuários
-app.post("/:empresaId/user/register", UserController.create);
-app.get("/:empresaId/user", UserController.list);
-app.get("/user/:userId", UserController.show);
-app.put("/user/:userId", UserController.update);
-
 //### Rotas para operações com empresas
 app.post("/company", CompanyController.create);
-app.put("/company/:companyId", CompanyController.update);
 app.get("/company", CompanyController.list);
 app.get("/company/:companyId", CompanyController.show);
+app.put("/company/:companyId", CompanyController.update);
 app.delete("/company/:companyId", CompanyController.delete);
+
+//### Rotas para operações com usuários
+app.post("/:empresaId/register", UserController.create);
+app.get("/:empresaId/user", UserController.list);
+app.get("/:empresaId/user/:userId", UserController.show);
+app.put("/:empresaId/user/:userId", UserController.update);
+app.delete("/:empresaId/user/:userId", UserController.delete);
 
 //### Rotas para operações com produtos
 app.post("/:companyId/products/", ProductController.create);
--- src/middlewares/AuthenticateUsers.js
+++ src/middlewares/AuthenticateUsers.js
@@ -4,21 +4,22 @@ const authConfig = require("../config/authConfig");
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader) return res.status(401).send({ error: "No token provider" });
+  if (!authHeader)
+    return res.status(401).send({ error: "Token não informado" });
 
   const parts = authHeader.split(" ");
 
   if (!parts.length === 2)
-    return res.status(401).send({ error: "Token error" });
+    return res.status(401).send({ error: "Erro no Token" });
 
   const [schema, token] = parts;
 
   if (!/^Bearer$/i.test(schema))
-    return res.status(401).send({ error: "Token malformated" });
+    return res.status(401).send({ error: "Token mau formado" });
 
   jwt.verify(token, authConfig.secret, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ error: err });
+      return res.status(401).send({ error: "Token inválido" });
     }
 
     req.userId = decoded.payload;
